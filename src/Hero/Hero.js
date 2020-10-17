c722f38afabf47746dc3109a230e28711b195460
kostacek <65678243+kostacek@users.noreply.github.com>
2020-10-16 22:17:38 -0400

    Brought PBudget code in

---
+++ src/Hero/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Hero() {
+  return (
+    
+  <div className="hero">
+  <h1>My Budget</h1>
+  <h2>A person-budget management app</h2>
+</div>
+  );
+}
+
+export default Hero;
---
+++ src/Menu/Menu.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import {
+    Link
+  } from "react-router-dom";
+
+function Menu() {
+  return (
+    <div>
+        <nav>
+            
+         <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/About">About</Link></li>
+          <li><Link to="/login">Login</Link></li>
+
+      </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Menu;
